126062d1e356f05cdfd20fa1789da9b1873491f7
yeong bin <2976622@naver.com>
2022-05-08 09:48:57 +0900

    day7: dfs(travel)-success

--- 08_DFS,BFS/L3_여행경로.js
+++ 08_DFS,BFS/L3_여행경로.js
@@ -1,32 +1,47 @@
-function dfs(graph, visit, key, answer){
-    visit.set(key, true);
-    answer.push(key);
-    let aaa = graph.get(key) || [];
-    for (let g of aaa){
-        if (!visit.get(g))
-            dfs(graph, visit, g, answer);
+/*
+지역이 아닌, 하나의 티켓을 노드라고 생각, [티켓의 목적지]->[티켓의 출발지]가 간선
+그리고, dfs
+*/
+let graph, visit, answer;
+
+function dfs(cur, count){
+    visit[cur] = true;
+    if (count === 1){
+        answer.push(cur);
+        return true;
     }
+    for (let next of graph[cur]){
+        if (!visit[next]){
+            let temp = dfs(next, count - 1);
+            if (temp){
+                answer.push(cur);
+                return true;
+            }
+        }
+    }
+    visit[cur] = false;
+    return false;
 }
 
 function solution(tickets) {
-    const answer = [];
-	const graph = new Map();
-    const visit = new Map();
-
-    for ([start, dest] of tickets){
-        let temp = graph.get(start);
-        if (temp){
-            temp.push(dest);
-            temp.sort();
-        }
-        else {
-            graph.set(start, [dest]);
-            visit.set(start, false);
-        }
+    answer = new Array();
+    graph = new Array(tickets.length);
+    visit = new Array(tickets.length);
+    //ticket을 노드로 하는 그래프 만들기 
+    tickets.sort((f, s) => (f[1] > s[1] ? 1 : (f[1] < s[1] ? -1 : 0)));
+    for (let i = 0; i < tickets.length; i++){
+        graph[i] = new Array();
+        tickets.forEach((t, j) => {if (t[0] === tickets[i][1]) graph[i].push(j);});
     }
-
-    dfs(graph, visit, "ICN", answer);
-    return answer;
+    //출발지가  ICN인 티켓(노드)만 dfs실행
+    for (let j = 0; j < tickets.length; j++){
+        if (tickets[j][0] == "ICN" && dfs(j, tickets.length))
+            break;
+    }
+    //들린 곳들만 추가해서 ICN 처음에 추가
+    let temp = answer.map(k => tickets[k][1]);
+    temp.push("ICN");
+    return temp.reverse();
 }
 
 console.log(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]));
