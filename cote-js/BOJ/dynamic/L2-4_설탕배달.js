ce3d084a832816fc469169081598947000bd6846
yeong bin <2976622@naver.com>
2022-07-03 23:17:05 +0900

    220703(1)

---
+++ cote-js/BOJ/dynamic/L2-4_설탕배달.js
@@ -0,0 +1,33 @@
+function solution(kg) {
+	let d = kg > 5 ? Array(kg + 1).fill(5001) : Array(6).fill(5001);
+	d[3] = 1;
+	d[5] = 1;
+	for (let k = 6; k <= kg; k++)
+		d[k] = Math.min(d[k - 3], d[k - 5]) + 1;
+	return d[kg] < 5001 ? d[kg].toString() : "-1";
+}
+
+//야매
+function solution2(kg) {
+	let answer = 0;
+	let count = 0;
+	while (true) {
+		if (0 > kg) {
+			answer = -1;
+			break;
+		}
+		if (kg % 5 === 0) {
+			answer = kg / 5 + count;
+			break;
+		}
+		count++;
+		kg -= 3;
+	}
+	return answer.toString();
+}
+
+// const line = require("fs").readFileSync("../input.txt").toString().trim().split('\n');
+const lines = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
+const kg = parseInt(lines[0]);
+
+console.log(solution(kg));
--- cote-js/BOJ/input.js
+++ cote-js/BOJ/input.js
@@ -1,5 +1,5 @@
 const lines = require("fs").readFileSync("./input.txt").toString().trim().split('\n');
-//const input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
+//const lines = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 let [n, _, start] = lines.shift().split(" ").map(item => parseInt(item));
 let edges = [];
 for (let line of lines)
